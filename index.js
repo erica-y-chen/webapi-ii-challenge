923cb9e9810ad5c7ef7e3cd0360e2eecd47d9eaa
erica-y-chen <eyufanchen@gmail.com>
2019-04-25 11:42:32 -0700

    Deleted package lock.json

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
+
 const server = require('./server.js')
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 server.listen(port, () => {
     console.log('\n*** API running on port 5k ***\n')
 })
